eb66d545fa55dbed5320ca0ed0ce97cd66ef2698
Kamil Luc <kamil.luc@edu.uekat.pl>
2025-11-16 11:39:37 +0100

    CU-86c6hv0rd - Implement create endpoint

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -3,10 +3,16 @@ from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     subtasks = serializers.SerializerMethodField()
+    parent = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all(), required=False, allow_null=True)
 
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'completed', 'parent', 'subtasks', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'subtasks']
 
     def get_subtasks(self, obj):
-        return TaskSerializer(obj.subtasks.all(), many=True).data
+        """Recursively serialize subtasks"""
+        subtasks = obj.subtasks.all()
+        if subtasks.exists():
+            return TaskSerializer(subtasks, many=True).data
+        return []
